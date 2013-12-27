9fb76f8e55f70a89de20dcab4d6ac39937cb2aaf
mckaymatt <mckaymatt@gmail.com>
2013-12-27 14:17:22 -0500

    display upgrades

--- VR_gif_booth.py
+++ VR_gif_booth.py
@@ -4,37 +4,45 @@ import sys
 import time
 import uuid
 import time
+import md5
+import string
+import random
 
 from SimpleCV import *
 
-global gif_frames
-global gif_frameRate 
+global gif_frames # Frames per .gif file
+global gif_frameRate # Frame rate for gif playback and gif record 
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        gif_frames = float(sys.argv[1])
+        # gif frame number and gif frame rate can be provided as arguments
+        gif_frames = float(sys.argv[1]) 
         gif_frameRate = float(sys.argv[2])
     else:
-        gif_frames = 8
+        gif_frames = 8.0 
         gif_frameRate = 0.4
-
-
-#class GifPlayback()
-  # Initialize the camera 
-# screen rez 1280 1024 
 
 class CameraCapture():
         
-    _key = True
-    _width = 320
-    _height = 240
+    _key = False
 
-    def __init__(self,index,width = 320 , height = 240):
+    _img_res_width = 320 
+    _img_res_height = 240
+    _screen_text = "Virtualizing!"
+
+    """
+    The images captured by the camera will scale to the resolution provided. 
+    """
+
+    def __init__(self,index,width = _img_res_width , height = _img_res_height):
         self.width = width
         self.height = height
         self.cam = Camera(index, prop_set={"width":self.width,"height":self.height})
+
         self.img_set = None 
         self.start_timer = None
+        self.screen_text1 = self._screen_text
+        self.output_text = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
             
     def setKey(self):
         if(self._key):
@@ -44,7 +52,7 @@ class CameraCapture():
 
     def makeGifSet(self):
         self.file_name = uuid.uuid4().hex # make a unique file name
-        self.img_set = ImageSet(directory=("output/"+ self.file_name  )) #init an imageClass.ImageSet()  
+        self.img_set = ImageSet(directory=("output/"+ self.file_name  )) # imageClass.ImageSet()  
         self.start_timer = time.time() #assign a value to start_timer
 
 
@@ -53,17 +61,17 @@ class CameraCapture():
             return False
         else:
             return True
+
     def fillSetThenSave(self, image):
         if self.img_set != None  and len(self.img_set) >= gif_frames:
-            print len(self.img_set)
             self.img_set._write_gif(filename=( "output/"+self.file_name + ".gif"),\
-                duration=(gif_frameRate), dither=2) #
+                duration=(gif_frameRate), dither=2) 
             return self.img_set
         if self.img_set != None:
             self.img_set.append(image)
             return None
 
-    #def playFromGif(self, ):
+
     def resetGifSet(self):
         self.start_timer = None # reassign start_timer and img_set 
         self.img_set = None # 
@@ -80,31 +88,106 @@ class CameraCapture():
 
     def getNewImage(self):
         img = self.cam.getImage()
-
         if(self._key):
             return img
         else:
-            img = img - img.colorDistance(Color.RED) #RED SEGMENTATION
+            img.getDrawingLayer().selectFont("andalemono")  
+            img.getDrawingLayer().circle(center=(self.width-30, self.height-30), radius=20, color=Color.RED, filled=True, alpha=100, antialias=10)      
+            #center, radius, color = Color.DEFAULT, width = 1, filled = False, alpha = -1, antialias = True)     
+            img.drawText(text = self.countDown(), x = 20, y = 10, color = Color.GOLD, fontsize = 24)
             return img
 
+    def countdown_engage(self, value):
+        self.final_text = "VIRTUALIZING"
+        value = map(lambda x : str(x), value)
+        for i in enumerate(self.final_text):
+            stored_char = value[i[0]]
+            random_char = random.choice(string.ascii_uppercase)
+            if self.output_text[i[0]] == self.final_text[i[0]]:
+                continue
+            if self.final_text[i[0]] == random_char:
+                self.output_text[i[0]] = random_char
 
 
-#make camera obj
+
+
+
+            #if self.final_text[i[0]] == random_char:
+                #self.output_text[i[0]] = random_char
+            #if value[i[0]] in string.ascii_letters:
+
+                
+
+
+            #if value[i[0]] == i[1]:
+            #    self.output_text[i[0]] = i[1] 
+            else:
+                self.output_text[i[0]] = value[i[0]]
+        if "".join(self.output_text) == self.final_text:
+            self.output_text = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+            return self.final_text
+        else:
+            return "".join(self.output_text)
+
+
+
+
+    def countDown(self):
+        self.screen_text1 = str(md5.new(self.screen_text1).hexdigest())[0:12] 
+        return self.countdown_engage(self.screen_text1)
+        #return self.screen_text1
+
+
+
+
+
+
+        # if "<" not in self.screen_text1:
+        #     self.screen_text1 = map(lambda x : str(x), self._screen_text)
+        #     self.screen_text1.append("<")
+        #     return "".join(self.screen_text1)
+        # elif self.screen_text1[0] == "<":
+        #     self.screen_text1 = self._screen_text
+        #     return self.screen_text1
+        # else:
+        #     self.screen_text1[self.screen_text1.index("<")-1] = "<"
+        #     return "".join(self.screen_text1)  
+
+class TimeController():
+    def __init__(self, interval, occurances=1):
+        self.start_time = time.time()
+        self.occurances = occurances
+
+    def check_timer(self):
+        if self.occurances < 1: 
+            return None
+        elif time.time() - self.start_time >= self.interval:
+            self.occurances -= 1
+            self.start_time = time.time()
+            return True
+        else:
+            return False
+
+
+
+
+# Init camera 
 cam1 = CameraCapture(1)
-#width = cam1.width
-#height = cam1.height
 
-disp = Display(flags=pg.FULLSCREEN ,title='Enter The Virtuality!' , resolution = (1280 , 1024))
+# Init display
+# The display resolution should match or fit within your display. The images capture
+# by the camera will be scaled to fit the display.
+disp = Display(flags=pg.FULLSCREEN , resolution = (1020 , 765))
 
-print("\n >>>Right click on the Image to exit!")
+print("\n >>>Press the ESC key to exit!")
 while disp.isNotDone():
+    # 1 Get image
     img1 = cam1.getNewImage()
     dwn = disp.leftButtonDownPosition()
-
+    # 2 Record Gif
     if dwn != None and cam1.gifSetExistsBool() == False:
         cam1.makeGifSet() # create file_name, img_set, start_timer  
-
-
+    # 3 Playback gif
     if cam1.frameTimer():
         gifSet = cam1.fillSetThenSave(img1)
         if gifSet != None:
@@ -117,19 +200,8 @@ while disp.isNotDone():
             else:
                 print "reset"
                 cam1.resetGifSet()
-
+    # 4 Display
     img1.save(disp)
-    #disp.writeFrame(img1, fit=False)
-
-    if disp.mouseRight:
-        pass
-
-
-
-
-
-
-
 
 print("\n >>>Program Exitted")
 quit()
